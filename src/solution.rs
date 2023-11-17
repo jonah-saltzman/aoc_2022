5087ad8e598493718ce2908974e7babf7fed1348
Jonah Saltzman <jonahsaltzman@gmail.com>
2023-11-17 14:09:40 -0500

    day2 part1

--- src/solution.rs
+++ src/solution.rs
@@ -3,7 +3,7 @@ const fn win_move(c: u8) -> u8 {
         b'A' => b'Y',
         b'B' => b'Z',
         b'C' => b'X',
-        _ => unreachable!()
+        _ => unreachable!(),
     }
 }
 
@@ -12,13 +12,13 @@ const fn move_score(c: u8) -> i32 {
         b'X' => 1,
         b'Y' => 2,
         b'Z' => 3,
-        _ => unreachable!()
+        _ => unreachable!(),
     }
 }
 
 #[derive(Default)]
 pub struct Calculator {
-    score: i32
+    score: i32,
 }
 
 impl Calculator {
@@ -26,7 +26,13 @@ impl Calculator {
         let opponent: u8 = line[0];
         let player: u8 = line[2];
         let is_win: bool = win_move(opponent) == player;
-        let game_score = if is_win { 6 } else if (player - 23) == opponent { 3 } else { 0 };
+        let game_score = if is_win {
+            6
+        } else if (player - 23) == opponent {
+            3
+        } else {
+            0
+        };
         let move_score = move_score(player);
         self.score += game_score + move_score;
     }
