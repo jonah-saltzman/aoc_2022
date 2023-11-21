e32b348a7b28a7772a4092624071079e931ca252
Jonah Saltzman <jonahsaltzman@gmail.com>
2023-11-21 15:00:55 -0500

    day3 part2

--- src/solution.rs
+++ src/solution.rs
@@ -4,33 +4,36 @@ fn get_priority(item: u8) -> i32 {
     match item {
         b'a'..=b'z' => (item - 96).into(),
         b'A'..=b'Z' => (item - 64 + 26).into(),
-        _ => unreachable!()
+        _ => unreachable!(),
     }
 }
 
 #[derive(Default)]
 pub struct Calculator {
     score: i32,
-    items: HashSet<u8>
+    items: HashSet<u8>,
+    state: usize,
 }
 
 impl Calculator {
     pub fn process_line(&mut self, line: &[u8]) {
-        let mid = line.len() / 2;
-        for &item in &line[..mid] {
-            self.items.insert(item);
-        }
-        let val = 'val: {
-            for item in &line[mid..] {
-                if self.items.contains(item) {
-                    break 'val *item
-                }
-            }
-            unreachable!()
+        let new_set: HashSet<u8> = if self.state == 0 {
+            line.iter().map(|&e| e).collect()
+        } else {
+            line.iter()
+                .filter_map(|e| self.items.get(e).map(|&e| e))
+                .collect()
         };
-        let priority = get_priority(val);
-        self.score += priority;
-        self.items.clear();
+        if self.state == 2 {
+            let item = *new_set.iter().next().unwrap();
+            let priority = get_priority(item);
+            self.score += priority;
+            self.items = HashSet::with_capacity(52);
+            self.state = 0;
+        } else {
+            self.items = new_set;
+            self.state += 1;
+        }
     }
 
     pub fn get_score(&self) -> i32 {
