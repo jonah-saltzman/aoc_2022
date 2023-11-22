ce0287ac6915e75553ec780bd0c858e2c49c5f0d
Jonah Saltzman <jonahsaltzman@gmail.com>
2023-11-22 18:18:23 -0500

    day6 part1

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,21 @@
-use std::io::{self, BufRead, BufReader};
+use std::io::{self, Read};
 
-mod parser;
 mod solution;
 
 use solution::Calculator;
 
 fn main() {
-    let mut buf = String::with_capacity(30);
-    let mut reader = BufReader::new(io::stdin().lock());
-    let mut init_state: Vec<Vec<char>> = vec![];
-    while let Ok(n) = reader.read_line(&mut buf) {
-        if buf == "\n" {
-            break;
-        }
-        let stack = parser::parse_init_state(&buf[..n - 1]);
-        init_state.push(stack);
-        buf.clear();
-    }
-    let mut calc = Calculator::new(init_state);
-    buf.clear();
-    while let Ok(n) = reader.read_line(&mut buf) {
+    let mut buf: [u8; 256] = [0; 256];
+    let mut reader = io::stdin().lock();
+    let mut calc = Calculator::new();
+    while let Ok(n) = reader.read(&mut buf) {
         if n == 0 {
+            println!("EOF without answer");
             break;
         }
-        let cmd = parser::parse_instructions(&buf[..n - 1]);
-        calc.handle_command(cmd);
-        buf.clear();
+        if let Some(ans) = calc.process_chars(&buf[..n]) {
+            println!("{}", ans);
+            break
+        }
     }
-    let ans: String = calc.get_answer().into_iter().collect();
-    println!("{}", ans);
 }
--- src/solution.rs
+++ src/solution.rs
@@ -1,30 +1,66 @@
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub struct Command {
-    pub qty: usize,
-    pub src: usize,
-    pub dst: usize,
+use std::collections::VecDeque;
+
+macro_rules! to_idx {
+    ($i:expr) => {
+        ($i - 97) as usize
+    };
 }
 
 pub struct Calculator {
-    state: Vec<Vec<char>>,
+    window: VecDeque<u8>,
+    counts: [u8; 26],
+    repeats: u8,
+    idx: usize
 }
 
 impl Calculator {
-    pub fn new(init: Vec<Vec<char>>) -> Self {
-        Self { state: init }
-    }
-
-    pub fn handle_command(&mut self, cmd: Command) {
-        let split_at = self.state[cmd.src - 1].len() - cmd.qty;
-        let moved = self.state[cmd.src - 1].split_off(split_at);
-        self.state[cmd.dst - 1].extend(moved);
-    }
-
-    pub fn get_answer(self) -> Vec<char> {
-        let mut answer = vec![];
-        for mut stack in self.state.into_iter() {
-            answer.push(stack.pop().unwrap());
+    pub fn new() -> Self {
+        Self {
+            window: VecDeque::new(),
+            counts: [0; 26],
+            repeats: 0,
+            idx: 0,
         }
-        answer
     }
+
+    fn push(&mut self, c: u8) {
+        let initial = self.counts[to_idx!(c)];
+        if initial == 1 {
+            self.repeats += 1;
+        }
+        self.counts[to_idx!(c)] = initial + 1;
+        self.window.push_back(c);
+        self.idx += 1;
+    }
+
+    fn pop(&mut self) {
+        let c = self.window.pop_front().unwrap();
+        let initial = self.counts[to_idx!(c)];
+        if initial == 2 {
+            self.repeats -= 1;
+        }
+        self.counts[to_idx!(c)] = initial - 1;
+    }
+
+    pub fn process_chars(&mut self, chars: &[u8]) -> Option<usize> {
+        for &c in chars {
+            self.push(c);
+            if self.idx <= 4 {
+                if self.is_finished() {
+                    return Some(self.idx)
+                }
+                continue;
+            }
+            self.pop();
+            if self.is_finished() {
+                return Some(self.idx)
+            }
+        }
+        None
+    }
+
+    pub fn is_finished(&self) -> bool {
+        self.repeats == 0 && self.idx >= 4
+    }
+
 }
